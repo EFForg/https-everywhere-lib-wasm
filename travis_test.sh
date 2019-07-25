133ab4b24d0ac9aa4ed79e6be160863a8064354d
William Budington <bill@eff.org>
2019-07-24 17:49:05 -0700

    Adding .travis.yml and travis_test.sh

---
+++ travis_test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [ "$TEST" == "wasm-pack-test" ]; then
+  docker run -e WBINDGEN_I_PROMISE_JS_SYNTAX_WORKS_IN_NODE=1 -it --rm -v $(pwd):/opt https-everywhere-lib-wasm wasm-pack test --node
+fi
+
+if [ "$TEST" == "deterministic-build-output" ]; then
+  docker run -it --rm -v $(pwd):/opt https-everywhere-lib-wasm bash -c 'wasm-pack build -t no-modules && test "`git diff pkg/https_everywhere_lib_wasm_bg.wasm | wc -l`" == "0"'
+fi
